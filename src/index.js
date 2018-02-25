b0034cd734273dd9f3ffae1d1f3a8c67897762c3
Anna Marfel <anna.marfel@gmail.com>
2018-02-25 18:13:14 +0300

    task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let equationNew = equation.replace(/\s/g, '');
+    let array = equationNew.split("*");
+
+    let a = array[0];
+
+    let arrayB = array[1].split('^2');
+    let b = arrayB[1];
+
+    let arrayC = array[2].split('');
+    arrayC.shift();
+    let c = arrayC.join('');
+
+    let res1 = Math.round((-b + Math.sqrt(Math.pow(Number(b), 2) - 4 * a * c)) / (2 * a));
+    let res2 = Math.round((-b - Math.sqrt(Math.pow(Number(b), 2) - 4 * a * c)) / (2 * a));
+
+    let results = [];
+
+    results.push(res1);
+    results.push(res2);
+
+    results.sort((a, b) => {
+        return a - b;
+    });
+    return results;
 }
